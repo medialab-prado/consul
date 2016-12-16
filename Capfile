753bd93185c88bf0cfbe2e0e4cc8feda9e3f8b6e
Fernando Blat <ferblape@gmail.com>
2016-12-16 17:39:48 +0100

    Populate staging deploy

--- Capfile
+++ Capfile
@@ -7,10 +7,10 @@ require 'capistrano/deploy'
 require "capistrano/bundler"
 require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
-#require 'capistrano/passenger'
+require 'capistrano/passenger'
 require 'capistrano/delayed_job'
-require 'whenever/capistrano'
-require 'rvm1/capistrano3'
+require 'capistrano/rbenv'
+
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,58 +1,12 @@
-# config valid only for current version of Capistrano
 lock '3.5.0'
 
-def deploysecret(key)
-  @deploy_secrets_yml ||= YAML.load_file('config/deploy-secrets.yml')[fetch(:stage).to_s]
-  @deploy_secrets_yml.fetch(key.to_s, 'undefined')
-end
+set :application, 'consul-legislation'
+set :repo_url, 'git@github.com:PopulateTools/consul.git'
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/cache')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.2'
+set :rbenv_path, '/home/ubuntu/.rbenv'
+set :delayed_job_workers, 1
 
-set :rails_env, fetch(:stage)
-set :rvm1_ruby_version, '2.3.2'
-
-set :application, 'consul'
-set :full_app_name, deploysecret(:full_app_name)
-
-set :server_name, deploysecret(:server_name)
-set :repo_url, 'https://github.com/consul/consul.git'
-
-set :scm, :git
-set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
-
-set :log_level, :info
-set :pty, true
-set :use_sudo, false
-
-set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp public/system public/assets}
-
-set :keep_releases, 5
-
-set :local_user, ENV['USER']
-
-set :delayed_job_workers, 2
-
-set(:config_files, %w(
-  log_rotation
-  database.yml
-  secrets.yml
-  unicorn.rb
-))
-
-set :whenever_roles, -> { :app }
-
-namespace :deploy do
-  before :starting, 'rvm1:install:rvm'  # install/update RVM
-  before :starting, 'rvm1:install:ruby' # install Ruby and create gemset
-  before :starting, 'install_bundler_gem' # install bundler gem
-
-  after :publishing, 'deploy:restart'
-  after :published, 'delayed_job:restart'
-
-  after :finishing, 'deploy:cleanup'
-end
-
-task :install_bundler_gem do
-  on roles(:app) do
-    execute "rvm use #{fetch(:rvm1_ruby_version)}; gem install bundler"
-  end
-end
+set :passenger_restart_with_touch, true
