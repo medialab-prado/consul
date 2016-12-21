29080d49966dc3f591aec98bd0fb5036254f01af
Fernando Blat <ferblape@gmail.com>
2016-12-21 08:52:35 +0100

    New repo url

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.5.0'
 
 set :application, 'consul-legislation'
-set :repo_url, 'git@github.com:PopulateTools/consul.git'
+set :repo_url, 'git@github.com:medialab-prado/consul.git'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/cache')
 set :rbenv_type, :user
