b0cbe3c434374091baa4087d18e42f0c7dbc28f9
Juan Salvador Pérez García <jsperezg@gmail.com>
2018-06-07 17:13:31 +0200

    Fix migration, atempt 2

--- db/migrate/20151028213830_add_unaccent_extension.rb
+++ db/migrate/20151028213830_add_unaccent_extension.rb
@@ -2,7 +2,7 @@ class AddUnaccentExtension < ActiveRecord::Migration
   def change
     begin
       execute "create extension if not exists unaccent"
-    rescue StandardError => e
+    rescue Exception => e
       puts "Could not create extension unaccent. Please contact with your system administrator: #{e}"
     end
   end
